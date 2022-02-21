8a060c680c2ac49ea7b34ec5b1c22624b75bc086
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-02-20 22:09:20 -0300

    220919022022

---
+++ CameraSeguePlayer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraSeguePlayer
+{
+    class CameraSeguePlayer : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 offSet;
+
+        [Range(0,1)]
+        public float suv = 0.2f;
+        public Transform player;
+
+        private void FixedUpdate()
+        {
+            transform.position = Vector3.Lerp(transform.position, player.position + offSet, suv);
+            transform.LookAt(player);
+        }
+    }
+}
